ee2071caf8e906d9950337ed006c5b2adff8066b
Hamza Kamran <hamzakamrn8@gmail.com>
2025-03-27 15:13:32 +0500

    contact me left right setup

--- Portfolio-Project/src/components/ContactSection/ContactMeMain.jsx
+++ Portfolio-Project/src/components/ContactSection/ContactMeMain.jsx
@@ -6,10 +6,10 @@ const ContactMeMain = () => {
   return (
     <div
       id="contact"
-      className="max-w-[1200px] mx-auto items-center justify-center mt-[100px]"
+      className="max-w-[1200px] mx-auto items-center justify-center mt-[100px] px-4"
     >
       <h2 className="text-6xl text-cyan mb-10 text-center">ContactMe</h2>
-      <div className="flex justify-between gap-24 bg-brown p-8 rounded-2xl md:flex-row sm:flex-col"> 
+      <div className="flex justify-between gap-24 bg-brown p-8 rounded-2xl md:flex-row sm:flex-col">
         <ContactMeLeft />
         <ContactMeRight />
       </div>
