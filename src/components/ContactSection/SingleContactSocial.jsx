71a11b1b328fd1a582b1fd0943019c3a4d566cad
hamzakamran456 <hamzakamrn8@gmail.com>
2025-04-05 01:42:09 +0500

    hover on social icon and resume pdf upload

--- src/components/ContactSection/SingleContactSocial.jsx
+++ src/components/ContactSection/SingleContactSocial.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SingleContactSocial = ({ Icon, Link }) => {
   return (
-    <div className="text-2xl w-12 h-12 border border-orange text-orange rounded-full p-3 flex items-center">
+    <div className="text-2xl w-12 h-12 border border-orange text-orange rounded-full p-3 flex items-center hover:bg-orange hover:text-white hover:scale-110 transition-all duration-500">
       <a
         href={Link}
         target="_blank"
