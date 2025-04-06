d4b7a3bb51210338a9064c66dcefde2acbf6a680
hamzakamran456 <hamzakamrn8@gmail.com>
2025-04-06 21:04:54 +0500

    heropic and resume update

--- src/components/HeroSection/HeroPic.jsx
+++ src/components/HeroSection/HeroPic.jsx
@@ -16,7 +16,7 @@ const HeroPic = () => {
       <img src={ProfileImg} alt="Hamza Kamran" className="max-h-[450px] w-auto" />
 
       <div className="absolute -z-10 flex justify-center items-center animate-pulse overflow-hidden">
-        <PiHexagonThin className="lg:block min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] md:hidden sm:hidden" />
+        <PiHexagonThin className="md:h-[90%] md:min-h-[400px] sm:h-[120%] sm:min-h-[480px] lg:min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]" />
       </div>
     </motion.div>
   );
